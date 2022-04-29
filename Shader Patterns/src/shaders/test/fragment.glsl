b062fc116907f57da4fae155245769e4f4521521
PatrickMunar <rtmunar@up.edu.ph>
2022-04-29 22:26:51 +0800

    Pattern Push

--- Shader Patterns/src/shaders/test/fragment.glsl
+++ Shader Patterns/src/shaders/test/fragment.glsl
@@ -1,4 +1,43 @@
+varying vec2 vUv;
+
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    // 0
+    // gl_FragColor = vec4(vUv.x, vUv.y, 1.0, 1.0);
+
+    // 1
+    // gl_FragColor = vec4(vUv.x, vUv.y, 0.0, 1.0);
+
+    // 2
+    // gl_FragColor = vec4(vUv.x, vUv.y, 0.5, 1.0);
+
+    // 3
+    // gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0);
+
+    // 4
+    // float strength = vUv.x;
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
+
+    // 5
+    // float strength = 1.0 - vUv.y;
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
+
+     // 6
+    // float strength = vUv.y * 10.0;
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
+    
+    // 7
+    // float strength = mod(vUv.y * 10.0, 1.0);
+    // gl_FragColor = vec4(strength, strength, strength, 1.0);
+
+    // 8
+    float strength = mod(vUv.y * 10.0, 1.0);
+    if (strength < 0.5) {
+        strength = 0.0;
+    }
+    else {
+        strength = 1.0;
+    }
+
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
